459622f8265d57386e56a3f152788f1471e5cca5
mikaelcaua <mikaelcauasilva1@gmail.com>
2026-01-07 10:22:15 -0300

    refactor: moving transparencia section after projects section

--- app/page.tsx
+++ app/page.tsx
@@ -13,8 +13,8 @@ export default function Home() {
       <Header />
       <Hero />
       <History />
-      <Transparencia />
       <Projects />
+      <Transparencia />
       <Contact />
       <Footer />
     </main>
